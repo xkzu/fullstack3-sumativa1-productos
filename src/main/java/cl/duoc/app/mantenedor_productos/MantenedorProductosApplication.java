007f311bbd3ca15eee766d2d3e851f87da1a010f
Kzu <alvaro.vega.pavez@gmail.com>
2024-10-28 19:27:51 -0300

    estructura inicial

---
+++ src/main/java/cl/duoc/app/mantenedor_productos/MantenedorProductosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.app.mantenedor_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantenedorProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantenedorProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/app/mantenedor_productos/MantenedorProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.app.mantenedor_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantenedorProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
